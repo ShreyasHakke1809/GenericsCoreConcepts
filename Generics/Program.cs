636dd2085d591a5e8fe9bc0a6eea5e013351ff85
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-20 15:38:27 +0530

    Adding to print array without generics program

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
+            int[] intArray = { 9, 18, 27, 36, 45 };
+            double[] doubleArray = { 9.99, 18, 27, 27.45, 45.54 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
             Console.WriteLine("Welcome to Generic Programs");
+            Console.WriteLine("Choose any one program from given option");
+            Console.WriteLine("1.Print array without generics");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    PrintArray.ToPrint(intArray);
+                    PrintArray.ToPrint(doubleArray);
+                    PrintArray.ToPrint(charArray);
+                    break;
+                default:
+                    Console.WriteLine("Choose number within range");
+                    break;
+            }
+
+
+           
         }    
     }
 }
